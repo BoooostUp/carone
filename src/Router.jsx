f7ee23c14b8bdef287db52acca56f9f413f77c97
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-25 22:20:42 +0900

    ✨ Feat: Updated router

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,4 +1,5 @@
 import { createBrowserRouter } from 'react-router-dom';
+import BnbTestPage from './components/Main/BNB/BnbTestPage';
 import Carone from './pages/Carone';
 import Ce from './pages/Ce';
 import Home from './pages/Home';
@@ -40,6 +41,10 @@ const Router = createBrowserRouter([
       {
         path: 'si',
         element: <Si />,
+      },
+      {
+        path: 'test',
+        element: <BnbTestPage />,
       },
     ],
   },
