e490287cc9dc21089ba0eaeccf04961d60241b3f
ggjiny <jnss599@gmail.com>
2024-03-25 18:35:34 +0900

    ♻️ Refactor: GNB_CONTENTS 객체 수정

--- src/constants/GNB_CONTENTS.js
+++ src/constants/GNB_CONTENTS.js
@@ -1,16 +1,29 @@
 export const GNB_CONTENTS = Object.freeze({
-  TITLE: {
-    HOME: ['Waste Good', 'Energy & Water'],
-    CE: ['자동차 환경', '폐기물 수집, 운반, 중간 처리'],
-    SG: ['(주)에스지 산업', '폐유를 재활용 이온 정제류를 생산'],
-    SI: ['세명인택(주)', '세정, 세척 등'],
-    TOTAL: [
-      '(주)토탈',
-      '산업 폐기물 소각부터 재활용하는 완벽 시스템',
-      '1공장',
-      '2공장',
-    ],
-    CARONE: ['(주)카원', '폐기물을 자원으로 최대한 재활용', '1공장', '2공장'],
+  HOME: {
+    Title: 'Waste Good',
+    SubTitle: 'Energy & Water',
+  },
+  CE: {
+    Title: '자동차 환경',
+    SubTitle: '폐기물 수집, 운반, 중간 처리',
+  },
+  SG: {
+    Title: '(주)에스지 산업',
+    SubTitle: '폐유를 재활용 이온 정제류를 생산',
+  },
+  SI: {
+    Title: '세명인택(주)',
+    SubTitle: '세정, 세척 등',
+  },
+  TOTAL: {
+    Title: '(주)토탈',
+    SubTitle: '산업 폐기물 소각부터 재활용하는 완벽 시스템',
+    Farctory: ['1공장', '2공장'],
+  },
+  CARONE: {
+    Title: '(주)카원',
+    SubTitle: '폐기물을 자원으로 최대한 재활용',
+    Farctory: ['1공장', '2공장'],
   },
   HOME_MENU: ['인사말', '사업 분야', '오시는 길', '인재 채용'],
   MENU: ['회사 소개', '사업 내용', '보유 설비', '인허가사항'],
