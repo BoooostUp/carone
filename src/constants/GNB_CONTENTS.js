3bc04427c18348bba6f1c00c757c3a60eb6deb56
ggjiny <jnss599@gmail.com>
2024-03-25 18:48:35 +0900

    🐛 Fix: GNB_CONTENTS 오타 수정

--- src/constants/GNB_CONTENTS.js
+++ src/constants/GNB_CONTENTS.js
@@ -18,12 +18,12 @@ export const GNB_CONTENTS = Object.freeze({
   TOTAL: {
     Title: '(주)토탈',
     SubTitle: '산업 폐기물 소각부터 재활용하는 완벽 시스템',
-    Farctory: ['1공장', '2공장'],
+    Factory: ['1공장', '2공장'],
   },
   CARONE: {
     Title: '(주)카원',
     SubTitle: '폐기물을 자원으로 최대한 재활용',
-    Farctory: ['1공장', '2공장'],
+    Factory: ['1공장', '2공장'],
   },
   HOME_MENU: ['인사말', '사업 분야', '오시는 길', '인재 채용'],
   MENU: ['회사 소개', '사업 내용', '보유 설비', '인허가사항'],
