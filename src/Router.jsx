c299feb8d1b76c64758108c319d4a494163cc11d
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2024-03-25 17:02:42 +0900

    ⚰️ Fix: Remove test router route.

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,5 +1,4 @@
 import { createBrowserRouter } from 'react-router-dom';
-import BnbTestPage from './components/Main/BNB/BnbTestPage';
 import Carone from './pages/Carone';
 import Ce from './pages/Ce';
 import Home from './pages/Home';
@@ -41,10 +40,6 @@ const Router = createBrowserRouter([
       {
         path: 'si',
         element: <Si />,
-      },
-      {
-        path: 'test',
-        element: <BnbTestPage />,
       },
     ],
   },
