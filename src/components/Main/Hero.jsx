5b401847bcc9c634889657a433c7c45dcd804141
JaeBeen95 <nin2348@gmail.com>
2024-03-30 20:07:25 +0900

    ✨ Feat: Hero파일 생성

---
+++ src/components/Main/Hero.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const Hero = () => {
+  return <S.Container />;
+};
+
+export default Hero;
+
+const S = {
+  Container: styled.div`
+    width: 100%;
+  `,
+};
