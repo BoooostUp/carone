2af3251f00f47fe1c65fc8709761ec0b48deea78
ggjiny <jnss599@gmail.com>
2024-03-30 22:19:14 +0900

    ✨ Feat: Map 구현

---
+++ src/constants/MAP_CONTENTS.js
@@ -0,0 +1,37 @@
+export const MAP_CONTENTS = Object.freeze({
+  CE: {
+    name: '자동차 환경',
+    address: '경상남도 양산시 상북면 공원로 403 - 19',
+    tel: '055. 381. 0703',
+    fax: '055. 384. 0703',
+    imageSrc: '/src/assets/images/map/CE_CARONE_SG.webp',
+  },
+  SG: {
+    name: '(주)에스지 산업',
+    address: '경상남도 양산시 상북면 공원로 403 - 19',
+    tel: '055. 374. 7784',
+    fax: '055. 374. 7786',
+    imageSrc: '/src/assets/images/map/CE_CARONE_SG.webp',
+  },
+  SI: {
+    name: '(주)세명인택',
+    address: '울산광역시 남구 용연로 98번길 23',
+    tel: '052. 277. 5550',
+    fax: '052. 970. 5800',
+    imageSrc: '/src/assets/images/map/TOTAL_SI.webp',
+  },
+  TOTAL: {
+    name: '(주)토탈',
+    address: '울산광역시 남구 용연로 179번길 18',
+    tel: '052. 256. 0207',
+    fax: '052. 256. 0209',
+    imageSrc: '/src/assets/images/map/TOTAL_SI.webp',
+  },
+  CARONE: {
+    name: '(주)카원',
+    address: '경상남도 양산시 상북면 공원로 403 - 19',
+    tel: '055. 381. 0703',
+    fax: '055. 384. 0703',
+    imageSrc: '/src/assets/images/map/CE_CARONE_SG.webp',
+  },
+});
