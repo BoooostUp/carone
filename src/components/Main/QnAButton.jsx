fa296cacd17184cacf6fdc1308fa2e90e1cfca0c
JeongSabibi <jsbb528@naver.com>
2024-04-01 01:15:45 +0900

    ✨ Feat: QnA 버튼 UI 구현
    Feat: QnA 버튼 UI 구현

---
+++ src/components/Main/QnAButton.jsx
@@ -0,0 +1,44 @@
+import styled from 'styled-components';
+import { media } from '../../styles/utils/mediaQuery';
+
+const QnAButton = () => {
+  return (
+    <div>
+      <S.Container>
+        <img src="/src/assets/icons/vector.svg" />
+        <span>Q n A</span>
+      </S.Container>
+    </div>
+  );
+};
+
+export default QnAButton;
+
+const S = {
+  Container: styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 3.6rem;
+    height: 3.6rem;
+    background-color: ${({ theme }) => theme.color.white};
+    filter: drop-shadow(2px 4px 8px rgba(0, 0, 0, 0.25));
+    border-radius: 50%;
+
+    span {
+      display: none;
+      margin-left: 1rem;
+      ${({ theme }) => theme.font.FONT16SB};
+
+      ${media.desktop`
+        display: inline;
+      `}
+    }
+
+    ${media.desktop`
+      width: 10.8rem;
+      height: 3.8rem;
+      border-radius: 80px;
+    `}
+  `,
+};
