f08df1f278485373cda04fe82c1a88dcdc376226
ggjiny <jnss599@gmail.com>
2024-04-06 02:13:26 +0900

    💄 Style: CategoryBar styled css 위치 변경

--- src/components/Main/CategoryBar.jsx
+++ src/components/Main/CategoryBar.jsx
@@ -1,6 +1,31 @@
 import { useState } from 'react';
 import styled from 'styled-components';
 
+const CategoryBar = ({ company, category, selectedButton }) => {
+  const [toggleButton, setToggleButton] = useState(
+    selectedButton || category[0],
+  );
+  const handleClick = (item) => {
+    setToggleButton(item);
+  };
+  return (
+    <S.Container>
+      <S.CategoryContainer>
+        {category.map((item) => (
+          <S.Category
+            key={item}
+            $company={company}
+            $isToggled={toggleButton === item}
+            onClick={() => handleClick(item)}
+          >
+            {item}
+          </S.Category>
+        ))}
+      </S.CategoryContainer>
+    </S.Container>
+  );
+};
+
 const S = {
   Container: styled.div`
     display: flex;
@@ -27,33 +52,6 @@ const S = {
     ${({ theme, $isToggled }) =>
       $isToggled ? theme.font.FONT16SB : theme.font.FONT16}
   `,
-};
-
-const CategoryBar = ({ company, category, selectedButton }) => {
-  const [toggleButton, setToggleButton] = useState(
-    selectedButton || category[0],
-  );
-
-  const handleClick = (item) => {
-    setToggleButton(item);
-  };
-
-  return (
-    <S.Container>
-      <S.CategoryContainer>
-        {category.map((item) => (
-          <S.Category
-            key={item}
-            $company={company}
-            $isToggled={toggleButton === item}
-            onClick={() => handleClick(item)}
-          >
-            {item}
-          </S.Category>
-        ))}
-      </S.CategoryContainer>
-    </S.Container>
-  );
 };
 
 export default CategoryBar;
