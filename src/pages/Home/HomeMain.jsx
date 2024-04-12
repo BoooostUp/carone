42b51f57bcce307a0244eb3af08133f26a91bf6a
JeongSabibi <jsbb528@naver.com>
2024-04-12 17:10:17 +0900

    🐛 Fix: IntroContent 컴포넌트 불러오도록 수정
    Fix: IntroContent 컴포넌트 불러오도록 수정

--- src/pages/Home/HomeMain.jsx
+++ src/pages/Home/HomeMain.jsx
@@ -1,14 +1,7 @@
-import MainArticle from '../../components/Main/MainArticle';
-import { MAIN_ARTICLE_CONTENTS } from '../../constants/MAIN_ARTICLE_CONTENTS.js';
+import IntroContent from '../../components/Main/IntroContent';
 
 function HomeMain() {
-  return (
-    <div>
-      {MAIN_ARTICLE_CONTENTS.HOME.map((item, index) => {
-        return <MainArticle key={index} currentCompany="HOME" item={item} />;
-      })}
-    </div>
-  );
+  return <IntroContent />;
 }
 
 export default HomeMain;
