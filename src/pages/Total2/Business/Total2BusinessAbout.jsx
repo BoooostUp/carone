9e1c142757094e22745e6eb464288ace7e996698
ggjiny <jnss599@gmail.com>
2024-04-13 19:43:19 +0900

    ✨ Feat: 토탈1, 2공장 BusinessLayout 내용 추가 작업

--- src/pages/Total2/Business/Total2BusinessAbout.jsx
+++ src/pages/Total2/Business/Total2BusinessAbout.jsx
@@ -1,5 +1,11 @@
+import processImg from '../../../assets/images/total2/businessAbout.png';
+
 function Total2BusinessAbout() {
-  return <div>Total2BusinessAbout</div>;
+  return (
+    <div>
+      <img src={processImg} alt="사업분야" style={{ width: '100%' }} />
+    </div>
+  );
 }
 
 export default Total2BusinessAbout;
