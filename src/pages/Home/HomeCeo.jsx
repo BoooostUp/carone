cfb448b995a7f2f0809e0551e7b00e3e2f9dbac4
ggjiny <jnss599@gmail.com>
2024-04-14 01:13:04 +0900

    ✨ Feat: home에 인사말, 오시는 길 페이지 구현, Map 수정

--- src/pages/Home/HomeCeo.jsx
+++ src/pages/Home/HomeCeo.jsx
@@ -1,5 +1,33 @@
+import styled from 'styled-components';
+import ceo from '../../assets/images/home/ceo.png';
+
 function Ceo() {
-  return <div>Ceo</div>;
+  return (
+    <S.Container>
+      <S.Image src={ceo} alt="home 인사말" />
+    </S.Container>
+  );
 }
 
 export default Ceo;
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    max-width: 70rem;
+    min-height: 40rem;
+    padding-top: 8rem;
+    padding-bottom: 8rem;
+    @media (max-width: 767px) {
+      padding: 5rem 3rem;
+      min-height: 30rem;
+    }
+  `,
+
+  Image: styled.img`
+    width: 100%;
+  `,
+};
