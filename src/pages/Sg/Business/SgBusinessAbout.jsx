ae6a84cf045e062ea2946d5b67ced88c1ae317d1
ggjiny <jnss599@gmail.com>
2024-04-13 19:20:32 +0900

    ✨ Feat: carone2, ce, sg BusinessLayout 작업

--- src/pages/Sg/Business/SgBusinessAbout.jsx
+++ src/pages/Sg/Business/SgBusinessAbout.jsx
@@ -1,5 +1,20 @@
-function SgBusinessAbout() {
-  return <div>About</div>;
-}
+import styled from 'styled-components';
+import SubCategoryBar from '../../../components/Main/SubCategoryBar';
 
+function SgBusinessAbout() {
+  return (
+    <S.Layout>
+      <SubCategoryBar company="SG" />
+    </S.Layout>
+  );
+}
+const S = {
+  Layout: styled.div`
+    margin-top: -8rem;
+
+    @media (max-width: 767px) {
+      margin-top: -5rem;
+    }
+  `,
+};
 export default SgBusinessAbout;
