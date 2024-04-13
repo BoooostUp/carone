d78d5a32b9aac061bddbce4065ab99e2ee58c62a
ggjiny <jnss599@gmail.com>
2024-04-13 19:29:25 +0900

    ✨ Feat: 세명인텍 BusinessLayout 작업

--- src/pages/Si/Business/SiBusinessPipe.jsx
+++ src/pages/Si/Business/SiBusinessPipe.jsx
@@ -1,5 +1,15 @@
+import processImg from '../../../assets/images/si/businessPipe.png';
+
 function SiBusinessPipe() {
-  return <div>SiBusinessPipe</div>;
+  return (
+    <div>
+      <img
+        src={processImg}
+        alt="하수관/횡주관 청소"
+        style={{ width: '100%' }}
+      />
+    </div>
+  );
 }
 
 export default SiBusinessPipe;
