dc8794c1d058bdcd0dc8b9210e3e120600a3ec10
JaeBeen95 <nin2348@gmail.com>
2024-05-13 21:43:31 +0900

    ✨ feat: total2 이미지 적용

--- src/constants/MAIN_ARTICLE_CONTENTS.js
+++ src/constants/MAIN_ARTICLE_CONTENTS.js
@@ -29,10 +29,10 @@ import homeImage5 from '/src/assets/images/mainArticle/si.webp';
 // import total1Image3 from '';
 // import total1Image4 from '';
 
-// import total2Image1 from '';
-// import total2Image2 from '';
-// import total2Image3 from '';
-// import total2Image4 from '';
+import total2Image1 from '/src/assets/images/total2/material.webp';
+import total2Image2 from '/src/assets/images/total2/shredding.webp';
+import total2Image3 from '/src/assets/images/total2/grinding.webp';
+import total2Image4 from '/src/assets/images/total2/magnetic_separation.webp';
 
 // import siImage1 from '';
 // import siImage2 from '';
@@ -174,28 +174,28 @@ export const MAIN_ARTICLE_CONTENTS = Object.freeze({
       title1: '원료입고 및 보관',
       content1:
         ' 제조 과정에서 필요한 원료를 안전하게 수령하고, 적절한 조건에서 저장하여 품질을 유지하는 활동입니다',
-      imageAddress: '',
+      imageAddress: total2Image1,
     },
     {
       id: 2,
       title1: '파쇄',
       content1:
         '재활용 과정에서 대형 폐기물을 작은 조각으로 쪼개어 처리 용이성을 높이는 과정으로, 이는 재활용 효율을 개선하고 추가 처리 과정을 용이하게 합니다',
-      imageAddress: '',
+      imageAddress: total2Image2,
     },
     {
       id: 3,
       title1: '분쇄',
       content1:
         '고체 재료를 더 작은 입자로 만드는 과정으로, 화학 반응의 표면적을 늘리거나, 특정 제품의 생산을 위해 사용됩니다',
-      imageAddress: '',
+      imageAddress: total2Image3,
     },
     {
       id: 4,
       title1: '마그네틱 선별',
       content1:
         '자성을 이용하여 혼합물에서 철과 같은 자성 금속을 분리하는 과정으로, 재활용 산업에서 폐기물에서 가치 있는 재료를 회수하는 데 중요한 역할을 합니다',
-      imageAddress: '',
+      imageAddress: total2Image4,
     },
   ],
   SI: [
