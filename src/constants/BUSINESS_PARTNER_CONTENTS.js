f990b1b9f5c51a9bc8d86159a12e096992ac50be
JaeBeen95 <nin2348@gmail.com>
2024-05-17 16:01:38 +0900

    feat: total 2공장 주요 거래처 이미지

--- src/constants/BUSINESS_PARTNER_CONTENTS.js
+++ src/constants/BUSINESS_PARTNER_CONTENTS.js
@@ -30,6 +30,13 @@ import total6 from '../assets/images/total/business_partner6.webp';
 import total7 from '../assets/images/total/business_partner7.webp';
 import total8 from '../assets/images/total/business_partner8.webp';
 import total9 from '../assets/images/total/business_partner9.webp';
+import total21 from '../assets/images/total2/business_partner1.webp';
+import total22 from '../assets/images/total2/business_partner2.webp';
+import total23 from '../assets/images/total2/business_partner3.webp';
+import total24 from '../assets/images/total2/business_partner4.webp';
+import total25 from '../assets/images/total2/business_partner5.webp';
+import total26 from '../assets/images/total2/business_partner6.webp';
+import total27 from '../assets/images/total2/business_partner7.webp';
 
 export const BUSINESS_PARTNER_CONTENTS = Object.freeze({
   CE: {
@@ -92,8 +99,16 @@ export const BUSINESS_PARTNER_CONTENTS = Object.freeze({
   TOTAL2: {
     id: 6,
     title:
-      '재생드럼 생산업체, 금강페인트, 현대중공업, 삼성중공업, 한화오션, HJ중공업,대한조선, 현대삼호조선 등',
-    imageAddress: [image, image, image],
+      '재생드럼 생산업체, 금강페인트, 현대중공업, 삼성중공업, 한화오션, HJ중공업, 대한조선, 현대삼호조선 등',
+    imageAddress: [
+      total21,
+      total22,
+      total23,
+      total24,
+      total25,
+      total26,
+      total27,
+    ],
   },
   SI: {
     id: 7,
