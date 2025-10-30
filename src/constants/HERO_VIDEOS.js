bde8797532165592d47a3b78ca4286dc9a0c917e
Hyeongkwon Kim <105695762+royxk@users.noreply.github.com>
2025-10-30 09:59:55 +0900

    add_video

---
+++ src/constants/HERO_VIDEOS.js
@@ -0,0 +1,26 @@
+import introContent1 from '../assets/videos/IntroContent1.mp4';
+
+export const HERO_VIDEOS = Object.freeze({
+  HOME: {
+    main: introContent1,
+  },
+  CARONE: {},
+  CE: {},
+  SG: {
+    main: introContent1,
+  },
+  TOTAL: {
+    main: introContent1,
+  },
+  SI: {
+    main: introContent1,
+  },
+  //TODO: 에너지스토리 정보 기입
+  ES: {
+    main: introContent1,
+  },
+  //TODO: 황지 정보 기입
+  HJ: {
+    main: introContent1,
+  },
+});
